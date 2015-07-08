c7b5ed06bb0a180f67fa6d057bc644d6fb3b348d
David Schueler <admin@schueler.homeip.net>
2015-07-08 22:51:17 +0200

    Added Icons and functionality to browse directories, load and cache thumbnails.

--- FlashAir/src/de/evil2000/flashaircommander/FileListAdapter.java
+++ FlashAir/src/de/evil2000/flashaircommander/FileListAdapter.java
@@ -1,36 +1,111 @@
 package de.evil2000.flashaircommander;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class FileListAdapter extends ArrayAdapter<HashMap<String, String>> {
+	private Context appContext;
+	private ArrayList<String> ImageExtensions = new ArrayList<String>();
+	private ArrayList<String> MovieExtensions = new ArrayList<String>();;
 
 	public FileListAdapter(Context context, ArrayList<HashMap<String, String>> objects) {
 		super(context, 0, objects);
+		appContext = context;
+		ImageExtensions.addAll(Arrays.asList("ANI", "ANIM", "APNG", "ART", "BMP", "BPG", "BSAVE", "CAL", "CIN", "CPC", "CPT", "DDS", "DPX", "ECW", "EXR",
+				"FITS", "FLIC", "FPX", "GIF", "HDRI", "HEVC", "ICER", "ICNS", "ICO", "CUR", "ICS", "ILBM", "JBIG", "JBIG2", "JNG", "JPEG", "JPEG2000",
+				"JPEG-LS", "JPEGXR", "MNG", "MIFF", "NRRD", "PAM", "PBM", "PGM", "PPM", "PNM", "PCX", "PGF", "PICTOR", "PNG", "PSD", "PSB", "PSP", "QTVR",
+				"RAS", "RBE", "JPEG-HDR", "SGI", "TGA", "TIFF", "TIFF", "EP", "TIFF", "IT", "WBMP", "WebP", "XBM", "XCF", "XPM", "XWD", "CIFF", "DNG", "AI",
+				"CDR", "CGM", "DXF", "EVA", "EMF", "Gerber", "HVIF", "IGES", "PGML", "SVG", "VML", "WMF", "Xar", "CDF", "DjVu", "EPS", "PDF", "PICT", "PS",
+				"SWF", "XAML", "CR2"));
+		MovieExtensions.addAll(Arrays.asList("3g2", "3gp", "4xm", "a64", "aac", "ac3", "act", "adf", "adts", "adx", "aea", "afc", "aiff", "alaw", "alsa",
+				"amr", "anm", "apc", "ape", "aqtitle", "asf", "asf_stream", "ass", "ast", "au", "avi", "avm2", "avr", "avs", "bethsoftvid", "bfi", "bin",
+				"bink", "bit", "bmv", "brstm", "c93", "caca", "caf", "cavsvideo", "cdg", "cdxl", "concat", "crc", "daud", "dfa", "dirac", "dnxhd", "dsicin",
+				"dts", "dtshd", "dv", "dv1394", "dvd", "dxa", "ea", "ea_cdata", "eac3", "epaf", "f32be", "f32le", "f4v", "f64be", "f64le", "fbdev", "ffm",
+				"ffmetadata", "film_cpk", "filmstrip", "flac", "flic", "flv", "framecrc", "framemd5", "frm", "g722", "g723_1", "g729", "gif", "gsm", "gxf",
+				"h261", "h263", "h264", "hls", "hls", "applehttp", "ico", "idcin", "idf", "iff", "ilbc", "image2", "image2pipe", "ingenient", "ipmovie",
+				"ipod", "ircam", "ismv", "iss", "iv8", "ivf", "jacosub", "jv", "latm", "lavfi", "libcdio", "libdc1394", "lmlm4", "loas", "lvf", "lxf", "m4v",
+				"matroska", "matroska", "webm", "md5", "mgsts", "microdvd", "mjpeg", "mkvtimestamp_v2", "mlp", "mm", "mmf", "mov", "mov", "mp4", "m4a", "3gp",
+				"3g2", "mj2", "mp2", "mp3", "mp4", "mpc", "mpc8", "mpeg", "mpeg1video", "mpeg2video", "mpegts", "mpegtsraw", "mpegvideo", "mpjpeg", "mpl2",
+				"mpsub", "msnwctcp", "mtv", "mulaw", "mv", "mvi", "mxf", "mxf_d10", "mxg", "nc", "nistsphere", "nsv", "null", "nut", "nuv", "ogg", "oma",
+				"paf", "pjs", "pmp", "psp", "psxstr", "pva", "pvf", "qcp", "r3d", "rawvideo", "rcv", "realtext", "rl2", "rm", "roq", "rpl", "rso", "rtp",
+				"rtsp", "s16be", "s16le", "s24be", "s24le", "s32be", "s32le", "s8", "sami", "sap", "sbg", "sdl", "sdp", "segment", "shn", "siff", "smjpeg",
+				"smk", "smoothstreaming", "smush", "sol", "sox", "spdif", "srt", "stream_segment", "ssegment", "subviewer", "subviewer1", "svcd", "swf", "tak",
+				"tedcaptions", "tee", "thp", "tiertexseq", "tmv", "truehd", "tta", "tty", "txd", "u16be", "u16le", "u24be", "u24le", "u32be", "u32le", "u8",
+				"vc1", "vc1test", "vcd", "video4linux2", "v4l2", "vivo", "vmd", "vob", "vobsub", "voc", "vplayer", "vqf", "w64", "wav", "wc3movie", "webm",
+				"webvtt", "wsaud", "wsvqa", "wtv", "wv", "x11grab", "xa", "xbin", "xmv", "xwma", "yop", "yuv4mpegpipe"));
 	}
-	
+
 	@Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-       // Get the data item for this position
-		HashMap<String, String> file = getItem(position);    
-       // Check if an existing view is being reused, otherwise inflate the view
-       if (convertView == null) {
-          convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_filelist, parent, false);
-       }
-       // Lookup view for data population
-       TextView lblLine1 = (TextView) convertView.findViewById(R.id.lblLine1);
-       TextView lblLine2 = (TextView) convertView.findViewById(R.id.lblLine2);
-       // Populate the data into the template view using the data object
-       lblLine1.setText(file.get("filename"));
-       lblLine2.setText(file.get("attributes"));
-       // Return the completed view to render on screen
-       return convertView;
-   }
+	public View getView(int position, View convertView, ViewGroup parent) {
+		ImageView imgThumb;
+		TextView lblLine1;
+		TextView lblLine2;
+		// Check if an existing view is being reused, otherwise inflate the view
+		if (convertView == null) {
+			convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_filelist, parent, false);
+			imgThumb = (ImageView) convertView.findViewById(R.id.imgThumbnail);
+			lblLine1 = (TextView) convertView.findViewById(R.id.lblLine1);
+			lblLine2 = (TextView) convertView.findViewById(R.id.lblLine2);
+			convertView.setTag(new ViewHolder(imgThumb, lblLine1, lblLine2));
+		} else {
+			ViewHolder viewHolder = (ViewHolder) convertView.getTag();
+			imgThumb = viewHolder.imgThumb;
+			lblLine1 = viewHolder.lblText1;
+			lblLine2 = viewHolder.lblText2;
+		}
+
+		HashMap<String, String> file = getItem(position);
+
+		// Check if file entry is a directory
+		if (file.get("attributes").contains("d")) {
+			imgThumb.setImageDrawable(ContextCompat.getDrawable(appContext, R.drawable.folder));
+		} else {
+			// Generate the cacheFilename fom dirpath, filename and size 
+			String cacheFilename = appContext.getCacheDir().getAbsolutePath() + File.separator + file.get("directory").replace("/", ".") + file.get("filename")
+					+ file.get("size");
+			// If file is already cached, use it as thumbnail
+			if ((new File(cacheFilename)).exists()) {
+				imgThumb.setImageDrawable(Drawable.createFromPath(cacheFilename));
+			} else {
+				// If file is not cached, display an icon instead.
+				String ext = file.get("filename").substring(file.get("filename").lastIndexOf(".")+1);
+				if (ImageExtensions.contains(ext.toUpperCase()))
+					imgThumb.setImageDrawable(ContextCompat.getDrawable(appContext, R.drawable.image));
+				else if (MovieExtensions.contains(ext.toLowerCase()))
+					imgThumb.setImageDrawable(ContextCompat.getDrawable(appContext, R.drawable.movie));
+				else
+					imgThumb.setImageDrawable(ContextCompat.getDrawable(appContext, R.drawable.file));
+			}
+
+		}
+
+		lblLine1.setText(file.get("filename"));
+		lblLine2.setText(file.get("date") + " " + file.get("time"));
+
+		return convertView;
+	}
+
+	private static class ViewHolder {
+		public final ImageView imgThumb;
+		public final TextView lblText1;
+		public final TextView lblText2;
+
+		public ViewHolder(ImageView imgThumb, TextView lblText1, TextView lblText2) {
+			this.imgThumb = imgThumb;
+			this.lblText1 = lblText1;
+			this.lblText2 = lblText2;
+		}
+	}
 }
--- FlashAir/src/de/evil2000/flashaircommander/FlashAirCommander.java
+++ FlashAir/src/de/evil2000/flashaircommander/FlashAirCommander.java
@@ -1,5 +1,9 @@
 package de.evil2000.flashaircommander;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
@@ -9,7 +13,10 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.LinearLayout.LayoutParams;
 import android.widget.ListView;
 import android.widget.ProgressBar;
@@ -19,32 +26,42 @@ public class FlashAirCommander extends ActionBarActivity {
 	private static final String APP_NAME = "FlashAirCommander";
 	private static final String flashAirHost = "192.168.8.14";
 	private FileListAdapter fileListAdapter;
+	private HashMap<String, String> dirUp = new HashMap<String, String>();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_flash_air_commander);
+
+		dirUp.put("directory", "/");
+		dirUp.put("filename", "..");
+		dirUp.put("size", "0");
+		dirUp.put("attributes", "d");
+		dirUp.put("date", "");
+		dirUp.put("time", "");
 		
 		ProgressBar progressBar = new ProgressBar(this);
-        progressBar.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, Gravity.CENTER));
-        progressBar.setIndeterminate(true);
-		
-        ListView lstView = (ListView) findViewById(R.id.lstView);
-        
+		progressBar.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, Gravity.CENTER));
+		progressBar.setIndeterminate(true);
+
+		ListView lstView = (ListView) findViewById(R.id.lstView);
+
 		lstView.setEmptyView(progressBar);
 		ViewGroup root = (ViewGroup) findViewById(android.R.id.content);
-        root.addView(progressBar);
-        
-        ArrayList<HashMap<String, String>> files = new ArrayList<HashMap<String,String>>();
-        
-        fileListAdapter = new FileListAdapter(this, files);
-        lstView.setAdapter(fileListAdapter);
+		root.addView(progressBar);
+
+		ArrayList<HashMap<String, String>> files = new ArrayList<HashMap<String, String>>();
+
+		fileListAdapter = new FileListAdapter(this, files);
+		fileListAdapter.setNotifyOnChange(true);
+		lstView.setAdapter(fileListAdapter);
+		lstView.setOnItemClickListener(onListItemClick());
 	}
 
 	@Override
 	protected void onStart() {
 		super.onStart();
-		updateFileListing("/DCIM/100CANON");
+		updateFileListing("/");
 	}
 
 	@Override
@@ -66,43 +83,118 @@ public class FlashAirCommander extends ActionBarActivity {
 		return super.onOptionsItemSelected(item);
 	}
 
+	private AdapterView.OnItemClickListener onListItemClick() {
+		return new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> aview, View view, int position, long arg3) {
+				HashMap<String,String> entry = fileListAdapter.getItem(position);
+				if (entry.get("attributes").contains("d")) {
+					// User selected a directory
+					if (entry.get("filename").equals("..")) {
+						// Go up one dir if user selected ".."
+						updateFileListing(entry.get("directory").substring(0, entry.get("directory").lastIndexOf("/")));
+					} else {
+						// Dive into selected directory
+						updateFileListing(entry.get("directory") + "/" + entry.get("filename"));
+					}
+				}
+			}
+
+		};
+	}
+
+	/**
+	 * Download the file listing from FlashAir via API, parse it and store the result in fileListAdapter. Also triggers the download of the thumbnails.
+	 * 
+	 * @param dir
+	 */
 	private void updateFileListing(String dir) {
+		final String d = dir;
 		// Create callback for httpTask
 		OnTask callback = new OnTask() {
 			@Override
 			public void onTaskCompleted(String doc) {
 				fileListAdapter.clear();
+				if (!(d.equals("") || d.equals("/"))) {
+					dirUp.put("directory", d);
+					fileListAdapter.add(dirUp);
+				}
 				ArrayList<HashMap<String, String>> files = parseFileListing(doc);
 				fileListAdapter.addAll(files);
+				downloadAndCacheThumbnails();
 			}
 		};
-		
-		if (dir == null) dir = "/";
-		if (dir.equals("")) dir = "/";
 
-		/*HttpData httpData = new HttpData();
+		if (dir == null)
+			dir = "/";
+		if (dir.equals(""))
+			dir = "/";
+
+		HttpData httpData = new HttpData();
 		NetworkTask nt = new NetworkTask(httpData, callback);
-		nt.execute("http://" + flashAirHost + "/command.cgi?op=100&DIR="+dir);*/
-		
-		ArrayList<HashMap<String, String>> files = parseFileListing(generateFakeFileListing(dir));
-		fileListAdapter.clear();
-		fileListAdapter.addAll(files);
-		fileListAdapter.notifyDataSetChanged();
+		nt.execute("http://" + flashAirHost + "/command.cgi?op=100&DIR=" + dir);
 	}
-	
-	private ArrayList<HashMap<String,String>> parseFileListing(String raw) {
-		ArrayList<HashMap<String,String>> filelist = new ArrayList<HashMap<String,String>>();
+
+	/**
+	 * Download the thumbnails for each entry stored in the fileListAdapter and store it in the applications cache folder.
+	 * 
+	 */
+	private void downloadAndCacheThumbnails() {
+		HttpData httpData = new HttpData();
+		httpData.retries = 0;
+		int c = fileListAdapter.getCount();
+		for (int i = 0; i < c; i++) {
+			HashMap<String, String> entry = fileListAdapter.getItem(i);
+			String directory = entry.get("directory");
+			String filename = entry.get("filename");
+			String path = directory + "/" + filename;
+			final String cacheFilename = getCacheDir().getAbsolutePath() + File.separator + directory.replace("/", ".") + filename + entry.get("size");
+			File cachedFile = new File(cacheFilename);
+
+			OnTask callback = new OnTask() {
+				@Override
+				public void onTaskCompleted(byte[] img) {
+					try {
+						FileOutputStream fos = new FileOutputStream(cacheFilename);
+						fos.write(img);
+						fos.close();
+						fileListAdapter.notifyDataSetChanged();
+					} catch (FileNotFoundException e) {
+						e.printStackTrace();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+
+				}
+			};
+			NetworkTask nt = new NetworkTask(httpData, callback);
+			if (!cachedFile.exists()) {
+				nt.execute("http://" + flashAirHost + "/thumbnail.cgi?" + path);
+			}
+		}
+	}
+
+	/**
+	 * Parse the file listing returned from the FlashAir API according to https://flashair-developers.com/en/documents/api/commandcgi/#100
+	 * 
+	 * @param raw
+	 * @return
+	 */
+	private ArrayList<HashMap<String, String>> parseFileListing(String raw) {
+		ArrayList<HashMap<String, String>> filelist = new ArrayList<HashMap<String, String>>();
+		if (raw == null)
+			return filelist;
 		
 		String[] lines = raw.split("\r\n");
 		for (String line : lines) {
 			if (line.equals("WLANSD_FILELIST"))
 				continue;
-			String [] parts = line.split(",");
-			HashMap<String, String> details = new HashMap<String,String>();
+			String[] parts = line.split(",");
+			HashMap<String, String> details = new HashMap<String, String>();
 			details.put("directory", parts[0].equals("") ? "/" : parts[0]);
 			details.put("filename", parts[1]);
 			details.put("size", parts[2]);
-			
+
 			Integer roh = Integer.parseInt(parts[3]);
 			String attribs = "";
 			attribs += ((roh & 32) >> 5) > 0 ? "a" : ""; // Archive
@@ -111,8 +203,8 @@ public class FlashAirCommander extends ActionBarActivity {
 			attribs += ((roh & 4) >> 2) > 0 ? "s" : ""; // System file
 			attribs += ((roh & 2) >> 1) > 0 ? "h" : ""; // Hidden
 			attribs += (roh & 1) > 0 ? "r" : ""; // Read only
-			details.put("attributes",attribs);
-			
+			details.put("attributes", attribs);
+
 			roh = Integer.parseInt(parts[4]);
 			Integer year = ((roh & 32256) >> 9) + 1980;
 			Integer month = (roh & 480) >> 5;
@@ -120,24 +212,30 @@ public class FlashAirCommander extends ActionBarActivity {
 			roh = Integer.parseInt(parts[5]);
 			Integer hour = (roh & 63488) >> 11;
 			Integer minute = (roh & 2016) >> 5;
-			Integer second = (roh & 31) * 2;	
-			details.put("date", year+"-"+(month < 10 ? "0" : "")+month+"-"+(day < 10 ? "0" : "")+day);
-			details.put("time", (hour < 10 ? "0" : "")+hour+":"+(minute < 10 ? "0" : "")+minute+":"+(second < 10 ? "0" : "")+second);
-			
+			Integer second = (roh & 31) * 2;
+			details.put("date", year + "-" + (month < 10 ? "0" : "") + month + "-" + (day < 10 ? "0" : "") + day);
+			details.put("time", (hour < 10 ? "0" : "") + hour + ":" + (minute < 10 ? "0" : "") + minute + ":" + (second < 10 ? "0" : "") + second);
+
 			filelist.add(details);
 		}
-		
+
 		return filelist;
 	}
-	
-	
+
+	/**
+	 * For debugging.
+	 * 
+	 * @param dir
+	 * @return
+	 */
 	private String generateFakeFileListing(String dir) {
 		Random r = new Random();
 		int numEntries = r.nextInt(20);
-		
+
 		String roh = "WLANSD_FILELIST\r\n";
 		for (int i = 0; i < numEntries; i++) {
-			roh += dir+",filename,"+String.valueOf(r.nextInt(Integer.MAX_VALUE))+","+String.valueOf(r.nextInt(32))+","+String.valueOf(r.nextInt(Integer.MAX_VALUE))+","+String.valueOf(r.nextInt(Integer.MAX_VALUE))+"\r\n";			
+			roh += dir + ",filename," + String.valueOf(r.nextInt(Integer.MAX_VALUE)) + "," + String.valueOf(r.nextInt(32)) + ","
+					+ String.valueOf(r.nextInt(Integer.MAX_VALUE)) + "," + String.valueOf(r.nextInt(Integer.MAX_VALUE)) + "\r\n";
 		}
 		return roh;
 	}
